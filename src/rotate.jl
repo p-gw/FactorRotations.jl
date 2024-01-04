2590ed1457fa0070132a114bcffe4aa862803777
Philipp Gewessler <p.gewessler@gmx.at>
2024-01-04 11:02:43 +0100

    add some docs

--- src/rotate.jl
+++ src/rotate.jl
@@ -1,39 +1,69 @@
 """
-    rotate(Λ, method::RotationMethod, kwargs...)
+    rotate(Λ, method::RotationMethod; kwargs...)
+    rotate!(Λ, method::RotationMethod; kwargs...)
 
 Perform a rotation of the factor loading matrix `Λ`.
 
 ## Keyword arguments
-- `atol`:
-- `alpha`:
-- `maxiter1`:
-- `maxiter2`:
+- `atol`: Sets the absolute tolerance for convergence of the algorithm (default: 1e-6).
+- `alpha`: Sets the inital value for alpha (default: 1).
+- `maxiter1`: Controls the number of maximum iterations in the outer loop of the algorithm
+              (default: 1000).
+- `maxiter2`: Controls the number of maximum iterations in the inner loop of the algorithm
+              (default: 10).
 - `init`: A k-by-k matrix of starting values for the algorithm.
-          If `init = nothing` (the default), random starting values will be generated.
+          If `init = nothing` (the default), the identity matrix will be used as starting
+          values.
 """
-function rotate(Λ, alg, args...; kwargs...)
-    rotation = _rotate(Λ, alg, args...; kwargs...)
+function rotate(Λ, method; kwargs...)
+    rotation = _rotate(Λ, method; kwargs...)
 
     @info """
     Rotation algorithm converged after $(length(rotation.iterations)) iterations.
-          algorithm: $(typeof(alg))
+          algorithm: $(typeof(method))
           criterion: $(last(rotation.iterations).Q)
     """
 
     return rotation.L
 end
 
-function rotate!(Λ, args...; kwargs...)
-    Λ .= rotate(Λ, args...; kwargs...)
+"""
+    rotate(Λ, method::RotationMethod; kwargs...)
+
+Perform a rotation of the factor loading matrix Λ and overwrite Λ with the rotated loading
+matrix.
+
+See also [`rotate`](@ref).
+"""
+function rotate!(Λ, method; kwargs...)
+    Λ .= rotate(Λ, method; kwargs...)
     return Λ
 end
 
+"""
+    IterationState
+
+A struct that holds the state of an iteration of the rotation algorithm.
+"""
 struct IterationState{T<:Real,V<:Real}
     alpha::T
     maxiter::Int
     Q::V
 end
 
+"""
+    RotationState
+
+A struct that holds the state of the rotation algorithm.
+
+## Fields
+- `init`: The initial rotation matrix
+- `A`: The initial factor loading matrix
+- `T`: The current state of the rotation matrix
+- `L`: The current state of the rotated loading matrix
+- `iterations`: A vector of [`IterationState`](@ref) that holds iteration states of the
+                optimization.
+"""
 mutable struct RotationState{T<:AbstractMatrix}
     init::T
     A::T
@@ -44,6 +74,12 @@ end
 
 RotationState(init, A) = RotationState(init, A, init, A * init, IterationState[])
 
+"""
+    _rotate(A::AbstractMatrix, method::RotationMethod{Orthogonal}; kwargs...)
+
+Implements the algorithm for factor rotation described in Bernaard & Jennrich (2005) for
+orthogonal factor rotation.
+"""
 function _rotate(
     A::AbstractMatrix{TV},
     method::RotationMethod{Orthogonal};
@@ -119,7 +155,11 @@ function initialize(init, A::AbstractMatrix{TV}) where {TV}
     return RotationState(T, A)
 end
 
-"compute the projection `Gp` of `G` for orthogonal matrices"
+"""
+    project!(Gp, T, G)
+
+Compute the projection `Gp` of `G` for orthogonal matrices.
+"""
 function project!(Gp, T, G)
     M = T' * G
     S = (M + M') ./ 2
@@ -127,4 +167,9 @@ function project!(Gp, T, G)
     return Gp
 end
 
+"""
+    isconverged(s, atol)
+
+determines the convergence status of the algorithm.
+"""
 isconverged(s, atol) = s < atol
