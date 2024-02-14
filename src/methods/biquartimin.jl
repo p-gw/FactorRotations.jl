0588772ffa55843511108eef519c7f7b2ecb3754
p-gw <p.gewessler@gmx.at>
2024-02-14 14:04:28 +0100

    basic implementation of bifactor rotation

---
+++ src/methods/biquartimin.jl
@@ -0,0 +1,26 @@
+"""
+    Biquartimin
+
+
+"""
+struct Biquartimin{RT} <: RotationMethod{RT}
+    function Biquartimin(; orthogonal = false)
+        T = orthogonal ? Orthogonal : Oblique
+        return new{T}()
+    end
+end
+
+function criterion_and_gradient(::Biquartimin, Λ::AbstractMatrix)
+    p, k = size(Λ)
+    n = k - 1
+
+    Λ₂ = @view Λ[:, 2:end]
+    Λ₂sq = Λ₂ .^ 2
+    N = Ones(n, n) - I(n)
+
+    Q = sum(Λ₂sq .* (Λ₂sq * N))
+
+    ∇Q = zeros(size(Λ))
+    ∇Q[:, 2:end] .= 4 * Λ₂ .* (Λ₂sq * N)
+    return Q, ∇Q
+end
--- src/methods/methods.jl
+++ src/methods/methods.jl
@@ -79,6 +79,7 @@ true
 isoblique(method::RotationMethod) = method isa RotationMethod{Oblique}
 
 include("biquartimax.jl")
+include("biquartimin.jl")
 include("component_loss.jl")
 include("crawford_ferguson.jl")
 include("cubimax.jl")
