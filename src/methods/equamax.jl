6daec4ba10f03d2e4e45b80e5c6dfbfa424d9e18
Alexey Stukalov <astukalov@seer.bio>
2024-04-29 21:44:52 -0700

    Equamax: fix typo

--- src/methods/equamax.jl
+++ src/methods/equamax.jl
@@ -2,7 +2,7 @@
     Equamax()
 
 *Equamax* is an orthogonal rotation method, which is equivalent to [`Oblimin`](@ref)
-rotation of *p × k* loadings matrix *Λ* with ``\\gamma = \\frac{p}{2}``.
+rotation of *p × k* loadings matrix *Λ* with ``\\gamma = \\frac{k}{2}``.
 """
 struct Equamax <: RotationMethod{Orthogonal} end
 
